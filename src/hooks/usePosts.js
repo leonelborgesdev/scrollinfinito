0ab89c27558fcb84075ec7bc77e9aa98b655bde8
leonelborgesdev <leonel03t@gmail.com>
2024-02-07 02:03:08 -0400

    usePosts

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -8,5 +8,30 @@ const usePosts = (pageNum = 1) => {
   const [error, setError] = useState({});
   const [hasNextPage, setHasNextPage] = useState(false);
 };
+useEffect(() => {
+  setIsLoading(true);
+  setIsError(false);
+  setError({});
+
+  const controller = new AbortController();
+  const { signal } = controller;
+
+  getPostPage(pageNum, { signal })
+    .then((data) => {
+      setResults((prev) => [...prev, ...data]);
+      setHasNextPage(Boolean(data.length));
+      setIsLoading(false);
+    })
+    .catch((e) => {
+      setIsLoading(false);
+      if (signal.aborted) return;
+      setIsError(true);
+      setError({ message: e.message });
+    });
+
+  return () => controller.abort();
+}, [pageNum]);
+
+return { isLoading, isError, error, results, hasNextPage };
 
 export default usePosts;

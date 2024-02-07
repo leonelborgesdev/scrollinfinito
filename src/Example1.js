ad58bca87b54afc6e7fd31621501e2c74e25ed4a
leonelborgesdev <leonel03t@gmail.com>
2024-02-07 02:08:33 -0400

    add example1

--- src/Example1.js
+++ src/Example1.js
@@ -1,4 +1,19 @@
+import { useState } from "react";
+import usePosts from "./hooks/usePosts";
+
 const Example1 = () => {
+  const [pageNum, setPageNum] = useState(1);
+  const { isLoading, isError, error, results, hasNextPage } = usePosts(pageNum);
+
+  if (isError) return <p className="center">Error: {error.message}</p>;
+
+  const content = results.map((post, i) => {
+    if (results.length === i + 1) {
+      console.log("last element");
+    }
+    return <Post key={post.id} post={post} />;
+  });
+
   return (
     <>
       <h1 id="top">
@@ -6,6 +21,7 @@ const Example1 = () => {
         <br />
         &infin; Ex. 1 - React only
       </h1>
+      {content}
       <p className="center">
         <a href="#top">Back to Top</a>
       </p>

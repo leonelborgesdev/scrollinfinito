50dde98564fc4abef8346bf4be37c14fb89619e7
leonelborgesdev <leonel03t@gmail.com>
2024-02-07 00:04:01 -0400

    usestate

---
+++ src/hooks/usePosts.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import { getPostPage } from "../api/axios";
+
+const usePosts = (pageNum = 1) => {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState({});
+  const [hasNextPage, setHasNextPage] = useState(false);
+};
+
+export default usePosts;
